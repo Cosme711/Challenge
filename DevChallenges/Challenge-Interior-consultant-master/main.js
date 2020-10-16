061665d0393cbce786f0fecbc534587297161511
Cosme711 <39126953+Cosme711@users.noreply.github.com>
2020-10-16 11:58:59 +0200

    Update1 16-10-20 Interior Consultant

--- DevChallenges/Challenge-Interior-consultant-master/main.js
+++ DevChallenges/Challenge-Interior-consultant-master/main.js
@@ -1,10 +1,14 @@
 const hamburger = document.querySelector('.hamburger');
 const navLinks = document.querySelector('.nav-links');
 const body = document.querySelector('body');
-const line = document.querySelector('.line__hamburger')
+const line = document.querySelector('.line__hamburger');
+const main = document.querySelector('.main');
+const footer = document.querySelector('footer');
 
 hamburger.addEventListener('click', () => {
 	navLinks.classList.toggle("open");
 	body.classList.toggle("open");
 	line.classList.toggle("open");
+	main.classList.toggle("open");
+	footer.classList.toggle("open");
 })
